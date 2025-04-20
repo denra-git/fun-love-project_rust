abb58acd65b5bbbe9976af0f5e59c58312ecc40d
denra-git <denra312mail@gmail.com>
2025-04-20 14:17:18 +0330

    adding limitted chances

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,9 @@
 use actix_web::{post, App, HttpResponse, HttpServer, Responder};
 use actix_files::Files;
 use rand::Rng;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+static CLICK_COUNT: AtomicUsize = AtomicUsize::new(0);
 
 
 // Handle button movement
@@ -8,18 +11,21 @@ use rand::Rng;
 async fn move_button() -> impl Responder {
     let mut rng = rand::rng();
     
-    let max_x = 300;  
-    let max_y = 300;  
+    let max_x = 400;  
+    let max_y = 400;  
     
-    let new_x = rng.random_range(100..=max_x);
-    let new_y = rng.random_range(100..=max_y);
+    let new_x = rng.random_range(-200..=max_x);
+    let new_y = rng.random_range(-200..=max_y);
 
+    let chance_number = CLICK_COUNT.fetch_add(1, Ordering::SeqCst) + 1;
+
+  if chance_number <=1 {
+     
     HttpResponse::Ok().body(format!(
-        r#" 
-        <button
+        r#"<button
           class="no_button"
           id="runaway-btn"
-          hx-trigger="mouseenter"
+          hx-trigger="click"
           hx-post="/move-button"
           hx-swap="outerHTML"
           style="
@@ -31,8 +37,37 @@ async fn move_button() -> impl Responder {
         >
           NO
         </button>"#,
-        new_x, new_y
-    ))
+        new_x, new_y))
+        
+  }else {
+    HttpResponse::Ok().body(format!(
+      r#" <div style="
+      position: fixed;
+      top: 50%;
+      left: 40%;
+      transform: translate(-30%, -50%);
+      background-color: rgb(161, 2, 2);
+      padding: 5rem 10rem;
+      border-radius: 8px;
+      box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
+      z-index: 1000;
+      min-width: 300px;
+      text-align: center;">
+      <h2 style="
+        font-size: 2rem;">
+        THIS IS A SERIOUS WARRNING</h2>
+      </h2>
+      <h3> click if you are ready for your last chance</h3>
+      <button style="
+        background: black;
+        padding: 20px">
+        another chance?
+      </button>
+    </div>"#,
+     ))
+  }
+
+    
 }
 
 
