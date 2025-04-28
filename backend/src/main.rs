3c97941f3413bb7bc79bddecf2b9058bd1f5ac05
denra-git <denra312mail@gmail.com>
2025-04-28 13:08:32 +0330

    clear code

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,6 +5,30 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 
 static CLICK_COUNT: AtomicUsize = AtomicUsize::new(0);
 
+fn generate_no_button(target:&str ,x: i32, y: i32) -> String {
+  format!(
+      r##"<button
+          id="no-btn"
+          hx-trigger="click"
+          hx-post="/handle-no-button"
+          hx-swap="outerHTML"
+          hx-target="#{}"
+          style="position: relative; left: {}px; top: {}px; transition: all 0.3s ease;">
+         NO
+        </button>"##,
+      target,x, y
+  )
+}
+
+// fn generate_message(title:&str,p:&str, class:&str) -> String {
+//   format!(
+//       r#"<div id="message-container" class="{}-btn-message">
+//            <h2>"{}"</h2>
+//            <h3>{}</h3>
+//          </div>"#,title,p,class
+//   )
+// }
+
 
 // Handle no button movement
 #[post("/handle-no-button")]
@@ -19,52 +43,26 @@ async fn handle_no_button() -> impl Responder {
 
     let chance_number = CLICK_COUNT.fetch_add(1, Ordering::SeqCst) + 1;
 
-  if chance_number < 4 {
-     
-    HttpResponse::Ok().body(format!(
-        r###"<button
-              id="no-btn"
-              hx-trigger="click"
-              hx-post="/handle-no-button"
-              hx-swap="outerHTML"
-              hx-target="#no-btn"
-              style="
-               position: relative;
-               left: {}px;
-               top: {}px;
-               transition: all 0.3s ease;">
-             NO
-            </button>"###,
-        new_x, new_y))
-        
-  } else {
-    HttpResponse::Ok().body(format!(
-      r###" <button
-              id="no-btn"
-              hx-trigger="click"
-              hx-post="/handle-no-button"
-              hx-swap="outerHTML"
-              hx-target="#message-container"
-              style="
-               position: relative;
-               left: 0px;
-               top: 0px;
-               transition: all 0.3s ease;">
-             NO
-            </button>
-            <div id="message-container" style=" background-color: rgb(161, 2, 2); z-index: 1192;">
-              <h2 style="font-size: 2rem;"> THIS IS A SERIOUS WARNING</h2>
-              <h3>  click if you are ready for your last chance  </h3>
-              <button 
-               hx-trigger="click"
-               hx-post="/handle_another_chance_btn"
-               hx-swap="outerHTML"
-               hx-target="#message-container"
-               style="background: black; font-size: 1rem;">
-                 another chance?  
-              </button>
-            </div>"###,
-     ))
+
+    match chance_number {
+        0 .. 4 =>  HttpResponse::Ok().body(generate_no_button("no-btn", new_x, new_y)),
+        4 => HttpResponse::Ok().body(format!("{}{}",generate_no_button("message-container", 0, 0),
+       format!(r###" <div id="message-container" class="{}-btn-message">
+                <h2> THIS IS A SERIOUS WARNING</h2>
+                <h3>  click if you are ready for your last chance  </h3>
+                <button 
+                 hx-trigger="click"
+                 hx-post="/handle_another_chance_btn"
+                 hx-swap="outerHTML"
+                 hx-target="#message-container"
+                 style="background: black; font-size: 1rem;">
+                   another chance?  
+                </button>
+              </div>"###,"no") )),
+        _=> HttpResponse::Ok().body(format!(
+          r#"<div id="message-container" class="{}-btn-message">
+                 <h2>"YOU ARE SO DEAD"</h2>
+               </div>"#,"last-no")),
   } 
 }
 
@@ -72,20 +70,18 @@ async fn handle_no_button() -> impl Responder {
 #[post("/handle_yes_button")]
 async fn handle_yes_button()->impl Responder {
   HttpResponse::Ok().body(format!(
-    r#"<div id="message-container" style=" background-color: #2e7d32; z-index: 1192;">
-           <h2 style=" font-size: 2rem;"> "It was perfectly obvious" </h2>
-           <h3> lucky you</h3>
-         </div>"#))
+    r#"<div id="message-container" class="{}-btn-message">
+           <h2>"It was perfectly obvious"</h2>
+           <h3>lucky you</h3>
+         </div>"#,"yes"))
 }
 
 //handle another chance button
 #[post("/handle_another_chance_btn")]
 async fn handle_another_chance_btn()->impl Responder {
-  HttpResponse::Ok().body(
-    r#"<div id="message-container" style=" background-color: #2e7d32; visibility: hidden;">
-           <h2 style=" font-size: 2rem;"> "It was perfectly obvious" </h2>
-           <h3> lucky you</h3>
-         </div>"#)
+  HttpResponse::Ok().body(format!(
+    r#"<div id="message-container" class="{}-btn-message"></div>"#,"another-chance")
+)
 }
 
 
